b61a9d0c173792eeeb13ab9a6a6f6e4fb5d8aa03
HARSH NAYAK <100467506+terMinator1812003@users.noreply.github.com>
2023-10-11 22:12:10 +0530

    Add files via upload

---
+++ CheckIf_N_andItsDoubleExist1346.java
@@ -0,0 +1,13 @@
+public class CheckIf_N_andItsDoubleExist1346 {
+    public boolean checkIfExist(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i != j && arr[i] == 2 * arr[j]) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
