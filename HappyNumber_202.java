739142e8c364e8da3b18b79d649e15e92ba3cc78
HARSH NAYAK <100467506+terMinator1812003@users.noreply.github.com>
2023-10-16 23:16:47 +0530

    Add files via upload

---
+++ HappyNumber_202.java
@@ -0,0 +1,17 @@
+public class HappyNumber_202 {
+    public boolean isHappy(int n) {
+        if (n <= 0) {
+            return false;
+        }
+        while (n >= 10) {
+            int sum = 0;
+            while (n != 0) {
+                sum += (n % 10) * (n % 10);
+                n /= 10;
+            }
+            n = sum;
+
+        }
+        return n == 1 || n == 7;
+    }
+}
