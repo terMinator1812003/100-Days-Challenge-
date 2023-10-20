95b35f1c7074959f31509b66154c5e282c329743
HARSH NAYAK <100467506+terMinator1812003@users.noreply.github.com>
2023-10-20 22:51:01 +0530

    Add files via upload

---
+++ FairCandySwap_888.java
@@ -0,0 +1,19 @@
+public class FairCandySwap_888 {
+
+    public int[] fairCandySwap(int[] A, int[] B) {
+        int sumA = 0, sumB = 0;
+        for (int i = 0; i < A.length; i++)
+            sumA += A[i];
+        for (int i = 0; i < B.length; i++)
+            sumB += B[i];
+        int dif = (sumA - sumB) / 2;
+        for (int i = 0; i < A.length; i++) {
+            for (int j = 0; j < B.length; j++) {
+                if (A[i] - B[j] == dif)
+                    return new int[] { A[i], B[j] };
+            }
+        }
+        return null;
+    }
+
+}
