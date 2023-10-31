73e9d2ace481dc7b601f7e3a14186774fc8107f1
HARSH NAYAK <100467506+terMinator1812003@users.noreply.github.com>
2023-10-31 21:27:53 +0530

    Add files via upload

---
+++ IntersectionofTwoArrays_349.java
@@ -0,0 +1,25 @@
+import java.util.HashSet;
+
+public class IntersectionofTwoArrays_349 {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        HashSet<Integer> set1 = new HashSet<>();
+        HashSet<Integer> set2 = new HashSet<>();
+        for (int i = 0; i < nums1.length; i++) {
+            set1.add(nums1[i]);
+        }
+        for (int i = 0; i < nums2.length; i++) {
+            set2.add(nums2[i]);
+        }
+
+        set1.retainAll(set2);
+
+        // Convert set to array
+        int[] result = new int[set1.size()];
+        int index = 0;
+        for (int num : set1) {
+            result[index++] = num;
+        }
+
+        return result;
+    }
+}
